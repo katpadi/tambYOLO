464b603ead90c5f38c8f29f4513f62adcdf6bf57
Kat <hello@katpadi.ph>
2014-12-12 19:31:17 +0800

    some changes after 48 years

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Some comments about the app
 
 import requests
 import random
-from flask import Flask, render_template, request, make_response, session, jsonify
+from flask import Flask, render_template, request, flash, make_response, session, jsonify
 from authomatic.adapters import WerkzeugAdapter
 from authomatic import Authomatic
 
@@ -13,7 +13,7 @@ from config import CONFIG
 
 app = Flask(__name__)
 
-# Temp lang naman
+# DO NOT DO THIS IN REAL LIFE!
 app.config['SECRET_KEY'] = 'holyshitthisisasecret'
 
 # Instantiate Authomatic.
@@ -22,62 +22,50 @@ authomatic = Authomatic(CONFIG, 'your secret string', report_errors=False)
 
 @app.route('/')
 def index():
-    """
-    Home handler
-    """
-
-    return render_template('index.html')
-
+  return render_template('index.html')
 
 @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
 def login(provider_name):
-    """
-    Login handler, must accept both GET and POST to be able to use OpenID.
-    """
+  response = make_response()
+  result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
 
-    # We need response object for the WerkzeugAdapter.
-    response = make_response()
+  # If there is no LoginResult object, the login procedure is still pending.
+  if result:
+    if result.user:
+      # We need to update the user to get more info.
+      result.user.update()
 
-    # Log the user in, pass it the adapter and the provider name.
-    result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
+      # The rest happens inside the template.
+    return render_template('yolo.html', tweet={})
 
-    # If there is no LoginResult object, the login procedure is still pending.
-    if result:
-        if result.user:
-          '''
-          Save session here
-          '''
-          session['credentials'] = result.user.credentials.serialize()
-          # We need to update the user to get more info.
-          result.user.update()
-
-        # The rest happens inside the template.
-        #return render_template('login.html', result=result)
-        return render_template('yolo.html', tweet={})
-
-    # Don't forget to return the response.
-    return response
-
+  # Don't forget to return the response.
+  return response
 
 @app.route('/yolo', methods=['GET', 'POST'])
 def yolo():
   hashtag  = request.args.get('hashtag')
   how_many = request.args.get('how_many') # Wala pa
-  url = 'https://api.twitter.com/1.1/search/tweets.json'
-  params = {
-    'q': hashtag,
-    'count': 20,
-  }
-  res = authomatic.access(session['credentials'], url, params=params)
+  tweet = None
+  tweets = []
 
-  if res.data['statuses']:
-    tweet=random.choice(res.data['statuses'])
-  else:
-    # None huhu
-    tweet = None
+  if hashtag:
+    url = 'https://api.twitter.com/1.1/search/tweets.json'
+    params = {
+      'q': hashtag,
+      'count': 20,
+    }
+    res = authomatic.access(session['credentials'], url, params=params)
 
-  #random.sample = if more than 1
-  return render_template('yolo.html', tweet=tweet, tweets=res.data)
+    if res.data['statuses']:
+      # Totally not random!
+      tweet=random.choice(res.data['statuses'])
+      tweets=res.data
+    else:
+      # None huhu
+      tweet = None
+
+  # random.sample = if more than 1
+  return render_template('yolo.html', tweet=tweet, tweets=tweets)
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+  app.run(port=5001, debug=True)
